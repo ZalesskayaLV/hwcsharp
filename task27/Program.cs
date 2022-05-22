3ac133bb7f5f785dec9b5bb0ca5fbc1390754067
Lyubov <zalesskayalyubov@gmail.com>
2022-05-23 01:06:02 +0400

    Add task 27

---
+++ task27/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 27. Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+452 -> 11
+82 -> 10
+9012 -> 12
+*/ 
+
+int GetSummNumberDigit (int num) //метод получает сумму цифр числа
+{
+    string number = Convert.ToString(num);
+    int summ = 0;
+    for (int count = 0; count < number.Length; count++);
+        summ = summ + number[count];
+    return summ;
+        
+}
+
+
+
+int GetNumber(string msg) //метод проверяет что ввели число
+{
+while(true)
+{
+Console.WriteLine(msg);
+string valueFromConsole = Console.ReadLine();
+
+    if(int.TryParse(valueFromConsole, out int number))
+    {
+            return number;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не число. Введите число .");
+    }
+}
+}
+
+
+
+int number = GetNumber("Введите число: ");
+
+int result = GetSummNumberDigit (number);
+
+Console.WriteLine ($"{number} -> {result}");
