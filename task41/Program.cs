25d8b8b63ab2fd162c11affe906051d692f8fc80
Lyubov <zalesskayalyubov@gmail.com>
2022-06-01 10:17:26 +0400

    add solution task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,45 +3,22 @@
 
 
 //метод считает количество введенных положительных чисел
-int GetCountPositivelength(int[] num)
+int GetCountPositive(string[] array)
 {
-    int count = 0;
-    for (int i = 0; i < num.Length; i++)
+    int countOfPositive = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        if (num[i] > 0)
-            count++;
+        if (int.TryParse(array[i], out int number) && number > 0)
+            countOfPositive++;
     }
-    return count;
-}
-
-//метод, который формирует массив из введенных с клавиатуры чисел
-int[] CreateArray(int length, string[] list)
-{
-    int[] arr = new int[length];
-
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = int.Parse(list[i].ToString());
-    }
-    return arr;
-}
-
-//метод переводит строку в число
-int GetNumber(string value)
-{
-    Console.WriteLine(value);
-    int number = int.Parse(Console.ReadLine());
-    return number;
+    return countOfPositive;
 }
 
 
 Console.WriteLine("Введите несколько чисел через пробел: ");
-string length = Console.ReadLine();
-string[] list = length.Split(" ");
+string numbers = Console.ReadLine();
+string[] list = numbers.Split(" ");
 
 
-int lengthArray = GetNumber("Введите количество введенных чисел: ");
-int[] arraylengthString = CreateArray(lengthArray, list);
-
-int result = GetCountPositivelength(arraylengthString);
+int result = GetCountPositive(list);
 Console.WriteLine($"Количество положительных чисел равно: {result}");
