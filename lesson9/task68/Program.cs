20ae4158f3938a9330561998dc0335e9cff21da1
Lyubov <zalesskayalyubov@gmail.com>
2022-06-16 23:37:20 +0400

    Formatted

--- lesson9/task68/Program.cs
+++ lesson9/task68/Program.cs
@@ -3,16 +3,16 @@ m = 2, n = 3 -> A(m,n) = 9*/
 
 int Akkermann(int m, int n)
 {
-    if (m==0)
-        return n+1;
-    if(n==0)
-        return Akkermann(m-1,1);
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Akkermann(m - 1, 1);
     else
-        return Akkermann((m-1),Akkermann(m,n-1));
-    
+        return Akkermann((m - 1), Akkermann(m, n - 1));
+
 }
 
 int m = 2;
 int n = 3;
-int akkValue = Akkermann(m,n);
+int akkValue = Akkermann(m, n);
 Console.WriteLine($"m = {m} n = {n} значение = {akkValue}");
